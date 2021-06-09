5afa20946b5c96ebb1ed788733a411fabe65a519
santiago <santiagotaschetta@gmail.com>
2021-06-09 20:16:08 -0300

    added helpers/object tests

---
+++ src/utils/helpers/object.js
@@ -0,0 +1,17 @@
+
+export const object = (object) => ({
+
+  reduce: function(callback, initialValue) {
+    return Object
+      .entries(object)
+      .reduce(callback, initialValue)
+  },
+
+  toDataDescriptor: function () {
+    return this.reduce((result, [key, value]) => {
+      result[key] = { value }
+      return result
+    }, {})
+  }
+  
+})
