899a7235bfcb1ae8c8b915174d285a8175a0e932
santiago <santiagotaschetta@gmail.com>
2021-06-12 13:03:00 -0300

    added useTable export to @utils/config/index

--- src/utils/composition/index.js
+++ src/utils/composition/index.js
@@ -1,5 +1,7 @@
-import { express } from '@utils/config'
+import { express, knex } from '@utils/config'
 
 import { useRouter as configRouter } from './router.js'
+import { useTable as configTable} from './table.js'
 
 export const useRouter = configRouter({ express })
+export const useTable = configTable({ knex })
