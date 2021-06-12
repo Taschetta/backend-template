d405840bb44a867502463b78ad718a4465052086
santiago <santiagotaschetta@gmail.com>
2021-06-12 13:01:51 -0300

    added @utils/composition/table.js

--- src/utils/composition/table.js
+++ src/utils/composition/table.js
@@ -1,20 +1,71 @@
 
-export const useTable = ({ knex }) => () => ({
+export const useTable = ({ knex }) => (name) => ({
 
-  filter: function() {
+  query: function({ id, equal, like } = {}) {
+    const query = knex.table(name)
 
+    if(id) query.where('id', id)
+
+    if(equal) Object.entries(equal).map(([column, value]) => {
+      query.where(column, value)
+    })
+
+    if(like) Object.entries(like).map(([column, value]) => {
+      query.where(column, 'like', `%${value}%`)
+    })
+
+    return query
   },
 
-  find: function() {
-
-  },
-
-  save: function() {
-
-  },
-
-  remove: function() {
+  filter: async function(query) {
+    let result = {} 
     
+    result = await this.query(query)
+    result = result.length == 1 ? result[0]: result
+
+    return result
+  },
+
+  save: async function(data) {
+    let result = {}
+
+    result = (data.id)
+      ? await this.update(data)
+      : await this.create(data)
+
+    return result
+  },
+
+  create: async function(data) {
+    let result = {}
+
+    result = await this
+      .query()
+      .insert(data)
+
+    return result[0]
+  },
+
+  update: async function(data) {
+    let id = data.id
+    let result = {}
+
+    result = await this
+      .query({ id })
+      .update(data, ['id'])
+
+    return result[0]
+  },
+
+  remove: async function(data) {
+    let id = data.id
+    let result = {}
+
+    result = await this
+      .query({ id })
+      .del(['id'])
+
+    return result[0]
   }
   
 })
--- tests/unit/utils/composition/table.test.js
+++ tests/unit/utils/composition/table.test.js
@@ -3,37 +3,189 @@ const { useTable } = require('@utils/composition/table.js')
 
 describe('the useTable module', () => {
   
+  let query = {}
   let knex = {}
   let table = {}
   let tableName = 'tests'
 
   beforeEach(() => {
-    knex = mockKnex()
+    query = mockQuery()
+    knex = mockKnex(query)
     table = useTable({ knex })(tableName)
   })
 
-  describe('the table object', () => {
+  describe('when table.query is called', () => {
 
-    it('has a filter function', () => {
-      expect(table.filter).toEqual(expect.any(Function))
+    it('creates a query from knex.table', () => {
+      table.query()
+      expect(knex.table).toHaveBeenCalledTimes(1)
+      expect(knex.table).toHaveBeenCalledWith(tableName)
     })
 
-    it('has a find function', () => {
-      expect(table.find).toEqual(expect.any(Function))
+    it('returns the query', () => {
+      const result = table.query()
+      expect(result).toBe(knex.table())
     })
 
-    it('has a save function', () => {
-      expect(table.save).toEqual(expect.any(Function))
+    describe('if it recibes { id }', () => {
+      
+      it('calls query.where("id", id)', async () => {
+        table.query({ id: 666 })
+        expect(knex.table().where).toHaveBeenNthCalledWith(1, 'id', 666)
+      })
+
+    })
+    
+    describe('if it recibes { equal }', () => {
+      
+      test('maps equal to query.where', () => {
+        table.query({ equal: { nombre: 'santiago', apellido: 'taschetta' } })
+              
+        expect(query.where).toHaveBeenNthCalledWith(1, 'nombre', 'santiago')
+        expect(query.where).toHaveBeenNthCalledWith(2, 'apellido', 'taschetta')
+      })
+
+    })
+    
+    describe('if it recibes { like }', () => {
+      
+      it('maps like to query.where', () => {
+        table.query({ like: { telefono: '+ 54 9 11', pais: 'argentina' } })
+        
+        expect(query.where).toHaveBeenNthCalledWith(1, 'telefono', 'like', '%+ 54 9 11%')
+        expect(query.where).toHaveBeenNthCalledWith(2, 'pais', 'like', '%argentina%')
+      })
+
     })
 
-    it('has a remove function', () => {
-      expect(table.remove).toEqual(expect.any(Function))
+  })
+
+  describe('when table.filter is called', () => {
+
+    beforeEach(() => {
+      table.query = jest.fn().mockResolvedValue([0, 1, 2])
     })
+    
+    it('passes params to query', async () => {
+      let params = { equal: { localidad: 'Moreno' } }
+      await table.filter(params)
+      expect(table.query).toHaveBeenCalledWith(params)
+    })
+
+    it('returns the result from the query', async () => {
+      const result = await table.filter()
+      expect(result).toEqual([0, 1, 2])
+    })
+
+    describe('if the result is a single item', () => {
+
+      beforeEach(() => {
+        table.query.mockResolvedValue([0])
+      })
+      
+      it('returns only that item', async () => {
+        const result = await table.filter()
+        expect(result).toBe(0)
+      })
+      
+    })
+    
+  })
+
+  describe('when table.save is called', () => {
+
+    describe('and it recibes an item with a nullable id', () => {
+      
+      let item = { nombre: 'Ruben', apellido: 'Rada' }
+
+      beforeEach(() => {
+        query.insert.mockResolvedValue([1])
+      })
+
+      it('inserts it to the table', async () => {
+        await table.save(item)
+        expect(query.insert).toHaveBeenCalledWith(item)
+      })
+
+      it('returns its id', async () => {
+        let result = await table.create(item)
+        expect(result).toBe(1)
+      })
+      
+    })
+      
+    describe('and it recibes an item with an id', () => {
+
+      let item = { id: 1, nombre: 'Luis Alberto', apellido: 'Spinetta' }
+
+      beforeEach(() => {
+        query.update.mockResolvedValue([1])
+      })
+
+      it('makes a query with its id', async () => {
+        await table.update(item)
+        expect(query.where).toHaveBeenCalledWith('id', 1)
+      })
+
+      it('updates the item', async () => {
+        await table.update(item)
+        expect(query.update).toHaveBeenLastCalledWith(item, ['id'])
+      })
+
+      it('returns its id', async () => {
+        const result = await table.update(item)
+        expect(result).toBe(1)
+      })
+      
+    })
+    
+  })
+
+  describe('when table.remove is called', () => {
+
+    let item = { id: 1, nombre: 'Litto', apellido: 'Nebbia' }
+
+    beforeEach(() => {
+      query.del.mockResolvedValue([1])
+    })
+
+    describe('and it recibes a single item', () => {
+      
+      it('filters by its id', async () => {  
+        await table.remove(item)        
+        expect(query.where).toHaveBeenCalledWith('id', 1)
+      })
+
+      it('deletes it from the table', async () => {
+        await table.remove(item)
+        expect(query.del).toHaveBeenCalledWith(['id'])
+      })
+
+      it('returns its id', async () => {
+        const result = await table.remove(item)
+        expect(result).toBe(1)
+      })
+      
+    })
+    
     
   })
   
 })
 
-function mockKnex() {
-
+function mockKnex(query) {
+  return { 
+    table: jest.fn(() => query) 
+  }
 }
+
+function mockQuery() {
+  return {
+    where: jest.fn().mockReturnThis(),
+    whereIn: jest.fn().mockReturnThis(),
+    first: jest.fn().mockReturnThis(),      
+    insert: jest.fn().mockReturnThis(),
+    update: jest.fn().mockReturnThis(),
+    del: jest.fn().mockReturnThis(),
+  }
+} 
